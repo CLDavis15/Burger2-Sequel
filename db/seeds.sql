178475dc9e9f033138df6fc52e1ad7ceb41b99f3
CLDavis15 <calebldavis0@gmail.com>
2019-05-03 16:34:52 -0400

    made the seeds and added to the database.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("The Baconator 2.0");
+INSERT INTO burgers (burger_name) VALUES ("The Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("The Pounder and a half");
