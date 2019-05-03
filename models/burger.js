f1b45082fa4f62e2bb3eabf112405385e5c29dc3
CLDavis15 <calebldavis0@gmail.com>
2019-05-03 15:57:37 -0400

    create a database in the schema file.

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+    
+
+module.exports = function(sequelize, DataTypes) {
+  var Burgers = sequelize.define("Burgers", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+
+  return Burgers;
+}
---
+++ routes/api-routes.js
@@ -0,0 +1,9 @@
+
+// Import the model to use its database functions.
+var db = require("../models");
+
+module.exports = function(app) {
+
+    
+}
+
