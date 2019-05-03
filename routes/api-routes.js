262fc2bd421bb0e758aa93caba1368eaa1ca3333
CLDavis15 <calebldavis0@gmail.com>
2019-05-03 16:05:34 -0400

    set up the api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,37 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+    app.get("/", function(req, res) {
+         db.Burger.findAll({
+
+         }).then(function(burgers) {
+          res.json(burgers);
+        });
+      });
+
+    app.post("/api/burger", function(req, res) {
+        db.Burger.create({
+
+          burger_name: req.body.burger_name
+
+        }).then(function(results) {
+          res.json(results);
+
+        })
+      });
+
+      app.put("/api/burger/:id", function(req, res) {
+      
+        db.Burgers.update({
+          devoured: req.body.devoured,
+        }, {
+          where: {
+            id: req.params.id
+          }
+        }).then(function(results) {
+          res.json(results);
+        });
     
+      });
 }
 
